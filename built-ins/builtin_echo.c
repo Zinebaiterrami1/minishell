2ea614c794d4d32af4764b7d50dc042b693bbfef
Zineb Ait-Errami <zait-err@c4r5p10.1337.ma>
2025-06-10 15:58:39 +0100

    echo...

--- built-ins/builtin_echo.c
+++ built-ins/builtin_echo.c
@@ -6,93 +6,90 @@
 /*   By: zait-err <zait-err@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 21:07:42 by zait-err          #+#    #+#             */
-/*   Updated: 2025/05/30 18:18:39 by zait-err         ###   ########.fr       */
+/*   Updated: 2025/06/10 15:57:10 by zait-err         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-// t_command **init_cmd(char **args)
+// void ft_echo(t_command *cmd)
 // {
 //     int i;
-//     t_command *head;
 //     t_command *tmp;
-    
+//     int fd;
+
+//     fd = 1;
 //     i = 1;
-//     head = NULL;
-//     tmp = NULL;
-//     while(args[i])
+//     tmp = cmd;
+//     if(tmp && ft_strncmp("-n", tmp->arg[i], ft_strlen("-n")) == 0)
 //     {
-//         t_command *new_node = malloc(sizeof(t_command));
-//         new_node->arg = args[i];
-//         new_node->
+//         i++;
+//         if(tmp && tmp->redir && tmp->redir->name)    
+//             open_file(tmp);
+//         while(tmp)
+//         {
+//             write(fd, tmp->arg[i], ft_strlen(tmp->arg[i]));
+//             if(tmp->arg[i] != NULL)
+//                 write(fd, " ", 1);
+//             i++;
+//         }
+//     }
+//     else
+//     {   
+//         if(tmp && tmp->redir && tmp->redir->name)    
+//             open_file(tmp);
+//         while(tmp->arg[i])
+//         {   
+//             write(fd, tmp->arg[i], ft_strlen(tmp->arg[i]));
+//             if(tmp->arg[i] != NULL)
+//                 write(fd, " ", 1);
+//             i++;
+//         }
+//         write(1, "\n", 1);
 //     }
 // }
 
 void ft_echo(t_command *cmd)
 {
     int i;
-    t_command *tmp;
+    int flag;
     int fd;
-
-    fd = 1;
+    int j;
+    
     i = 1;
-    tmp = cmd;
-
-        if(tmp && ft_strncmp("-n", tmp->arg[i], ft_strlen("-n")) == 0)
+    flag = 1;
+    fd = 1;
+    // if(cmd->arg[i] && ft_strcmp(cmd->arg[i], "-n") == 0)
+    // {
+    //     flag = 0;
+    //     i++;
+    // }
+    if(cmd->arg[i] && cmd->arg[i][0] == '-' && cmd->arg[i][1] == 'n')
+    {
+        j = 2;
+        while(cmd->arg[i][j] == 'n')
+            j++;
+        if(cmd->arg[i][j] == '\0')
         {
-            i++;
-            while(tmp)
-            {
-                if(tmp->arg[i])
-                    write(1, tmp->arg[i], ft_strlen(tmp->arg[i]));
-                if(tmp->next_com != NULL)
-                    write(1, " ", 1);
-                i++;
-                tmp = tmp->next_com;
-            }
-        }
-        else
-        {
-            while(tmp)
-            {   
-                if(tmp && tmp->redir && tmp->redir->name)    
-                    open_file(tmp);
-                if(tmp->arg[i])
-                    write(1, tmp->arg[i], ft_strlen(tmp->arg[i]));
-                if(tmp->next_com != NULL)
-                    write(1, " ", 1);
-                i++;
-                tmp = tmp->next_com;
-            }
-            write(1, "\n", 1);
-        }
-        //Handle redirection if present
-        if(tmp && tmp->redir && tmp->redir->name)
-        {
-            if(tmp->redir->type == T_RED_OUT) // >
-                fd = open(tmp->redir->name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-            else if(tmp->redir->type == T_RED_OUT_APEND) // >>
-                fd = open(tmp->redir->name, O_CREAT | O_WRONLY | O_APPEND, 0644);
-            if(fd < 0)
-                printf("error fd\n");
-        }
-        while(tmp && tmp->arg[i])
-        {
-            write(fd, tmp->arg[i], ft_strlen(tmp->arg[i]));
-            if(tmp->arg[i + 1])
-                write(fd, " ", 1);
+            flag = 0;
             i++;
         }
-        if(fd != 1)
-            close(fd);
+    }
+    if(cmd && cmd->redir && cmd->redir->name)
+    {
+        if(open_file(cmd) == -1)
+            return ;
+    }
+    while(cmd->arg[i])
+    {
+        write(fd, cmd->arg[i], ft_strlen(cmd->arg[i]));
+        if(cmd->arg[i + 1])
+            write(1, " ", 1);
+        i++;
+    }
+    if(flag == 1)
+        write(1, "\n", 1);
 }
-
-// void ft_echo_in_file(int fd)
-// {
-
-// }
-
 
 /*
 1. write(1, &cmd->args[i], 1); is wrong
