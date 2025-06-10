5547458bb0e24cf36d603ed0ab1e36a3993da18e
Zineb Ait-Errami <zait-err@c4r5p10.1337.ma>
2025-06-10 19:09:25 +0100

    export...

--- built-ins/builtin_export.c
+++ built-ins/builtin_export.c
@@ -6,24 +6,85 @@
 /*   By: zait-err <zait-err@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 12:34:56 by zait-err          #+#    #+#             */
-/*   Updated: 2025/06/03 17:23:01 by zait-err         ###   ########.fr       */
+/*   Updated: 2025/06/10 18:46:29 by zait-err         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int is_valid_identifier(const char *str)
-{
-	int i = 0;
+// int is_valid_identifier(const char *str)
+// {
+// 	int i = 0;
 
-	if (!str || (!ft_isalpha(str[0]) && str[0] != '_'))
-		return (0);
-	while (str[i] && str[i] != '=')
+// 	if (!str || (!ft_isalpha(str[0]) && str[0] != '_'))
+// 		return (0);
+// 	while (str[i] && str[i] != '=')
+// 	{
+// 		if (!ft_isalnum(str[i]) && str[i] != '_')
+// 			return (0);
+// 		i++;
+// 	}
+// 	return (1);
+// }
+
+static void	free_key_value(char *key, char *value)
+{
+	free(key);
+	if (value)
+		free(value);
+}
+
+static void	add_export_var(t_env **env, char *arg)
+{
+	char	*key;
+	char	*value;
+	char	*equal;
+	t_env *tmp;
+
+	tmp = *env;
+	equal = ft_strchr(arg, '=');
+	if (equal)
 	{
-		if (!ft_isalnum(str[i]) && str[i] != '_')
-			return (0);
-		i++;
+		key = ft_substr(arg, 0, equal - arg);
+		value = ft_strdup(equal + 1);
+		if (!*key)
+		{	
+			printf("export: `%s`: not a valid identifier\n", arg);
+			free_key_value(key, value);
+			return ;
+		}
 	}
+	else
+	{
+		key = ft_strdup(arg);
+		value = NULL;
+	}
+	// check_dup(tmp, &arg);
+	set_env_value(env, key, value);
+}
+
+
+int is_valid_identifier(char *str, t_env **env)
+{
+	int i;
+	
+	i = 0;
+	while(str[i] != '=' && str[i] != '\0')
+	{
+		if(ft_isdigit(str[0]) == 1)
+			break;
+		while(ft_isalpha(str[i]) == 1 || ft_isdigit(str[i]) == 1 || str[i] == '_')
+			i++;
+		if(str[i] == '=' || str[i] == '\0')
+			return (0);
+		else if(str[i] == '+' && str[i + 1] == '=')
+		{
+			g_exit_status = 0;
+			return (add_export_var(env, str), 1);
+		}
+		break;
+	}
+	g_exit_status = 1;
 	return (1);
 }
 
@@ -70,7 +131,7 @@ static void print_export(t_env* export)
 		}
 		else
 		{
-			printf("key: %s, value: %s, line: %s\t", min->env_key, min->env_value, min->line);
+			// printf("key: %s, value: %s, line: %s\t", min->env_key, min->env_value, min->line);
 			printf("declare -x %s=\"%s\"\n", min->env_key, min->env_value);
 		}	
 		size--;
@@ -149,74 +210,39 @@ static void print_export(t_env* export)
 
 // helpers_export.c
 
-static void	free_key_value(char *key, char *value)
-{
-	free(key);
-	if (value)
-		free(value);
-}
-
-static int check_dup(t_env *lst, char **arg)
-{
-	// t_command *tmp;
-	t_env *current;
-	char *equal;
-	char *key;
+// static int check_dup(t_env *lst, char **arg)
+// {
+// 	// t_command *tmp;
+// 	t_env *current;
+// 	char *equal;
+// 	char *key;
 	
-	current = lst;
-	int i = 1;
+// 	current = lst;
+// 	int i = 1;
 
-	while(arg[i])
-	{	
-		equal = ft_strchr(arg[i], '=');
-		if(equal)
-		{			
-			key = ft_substr(arg[i], 0, equal - arg[i]);
-			current = lst;
-			while(current)
-			{
-				if(ft_strcmp(key, current->env_key) == 0)
-				{
-					free(key);
-					return (1);
-				}
-				current = current->next;
-			}
-			free (key);
-		}
-		i++;
-	}
-	return (0);
-}
+// 	while(arg[i])
+// 	{	
+// 		equal = ft_strchr(arg[i], '=');
+// 		if(equal)
+// 		{			
+// 			key = ft_substr(arg[i], 0, equal - arg[i]);
+// 			current = lst;
+// 			while(current)
+// 			{
+// 				if(ft_strcmp(key, current->env_key) == 0)
+// 				{
+// 					free(key);
+// 					return (1);
+// 				}
+// 				current = current->next;
+// 			}
+// 			free (key);
+// 		}
+// 		i++;
+// 	}
+// 	return (0);
+// }
 
-static void	add_export_var(t_env **env, char *arg)
-{
-	char	*key;
-	char	*value;
-	char	*equal;
-	t_env *tmp;
-
-	tmp = *env;
-	equal = ft_strchr(arg, '=');
-	if (equal)
-	{
-		key = ft_substr(arg, 0, equal - arg);
-		value = ft_strdup(equal + 1);
-		if (!*key)
-		{
-			printf("export: `%s`: not a valid identifier\n", arg);
-			free_key_value(key, value);
-			return ;
-		}
-	}
-	else
-	{
-		key = ft_strdup(arg);
-		value = NULL;
-	}
-	check_dup(tmp, &arg);
-	set_env_value(env, key, value);
-}
 
 static void	handle_export_args(t_env **env, t_command *cmd)
 {
@@ -225,10 +251,10 @@ static void	handle_export_args(t_env **env, t_command *cmd)
 	i = 1;
 	while (cmd->arg[i])
 	{
-		if (!is_valid_identifier(cmd->arg[i]))
+		if (is_valid_identifier(cmd->arg[i], env) == 1)
+		{
 			printf("export: `%s`: not a valid identifier\n", cmd->arg[i]);
-		else
-			add_export_var(env, cmd->arg[i]);
+		}
 		i++;
 	}
 }
